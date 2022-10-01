31bde927afd042f84dd2bed5e069f2c0cc16bb14
Dmitriy Born <1111born1111@gmail.com>
2022-10-01 15:06:13 +0300

    Ex_1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задание №1
+Console.WriteLine("Введите первое число: ");
+int Num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int Num2 = int.Parse(Console.ReadLine()!);
+
+if (Num1 > Num2){
+    Console.WriteLine($"Максимальное число из {Num1} и {Num2} - это {Num1}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число из {Num1} и {Num2} - это {Num2}");
+}
