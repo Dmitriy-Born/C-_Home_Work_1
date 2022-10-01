935bb2d729ddf1f2faaecfaccdf97d2b21254f73
Dmitriy Born <1111born1111@gmail.com>
2022-10-01 15:16:56 +0300

    Ex_2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,38 @@
 ﻿//Задание №1
+// Console.WriteLine("Введите первое число: ");
+// int Num1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите второе число: ");
+// int Num2 = int.Parse(Console.ReadLine()!);
+
+// if (Num1 > Num2){
+//     Console.WriteLine($"Максимальное число из {Num1} и {Num2} - это {Num1}");
+// }
+// else
+// {
+//     Console.WriteLine($"Максимальное число из {Num1} и {Num2} - это {Num2}");
+// }
+
+//Задание №2
 Console.WriteLine("Введите первое число: ");
 int Num1 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе число: ");
 int Num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int Num3 = int.Parse(Console.ReadLine()!);
 
-if (Num1 > Num2){
-    Console.WriteLine($"Максимальное число из {Num1} и {Num2} - это {Num1}");
+if (Num1 > Num2) {
+    if (Num1 > Num3){
+        Console.WriteLine($"Максимальное число из {Num1}, {Num2} и {Num3} - это {Num1}");
+    }
+    else {
+        Console.WriteLine($"Максимальное число из {Num1}, {Num2} и {Num3} - это {Num3}");
+    }
 }
-else
-{
-    Console.WriteLine($"Максимальное число из {Num1} и {Num2} - это {Num2}");
+else {
+    if (Num2 > Num3){
+        Console.WriteLine($"Максимальное число из {Num1}, {Num2} и {Num3} - это {Num2}");
+    }
+    else {
+        Console.WriteLine($"Максимальное число из {Num1}, {Num2} и {Num3} - это {Num3}");
+    }
 }
